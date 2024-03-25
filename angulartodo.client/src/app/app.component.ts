add19d843405d9279375743fda9e8b4504e37fe9
Elianora Rose <developer@elianorarose.com>
2024-03-25 13:59:24 -0400

    Quick and dirty test of the proxy

--- angulartodo.client/src/app/app.component.ts
+++ angulartodo.client/src/app/app.component.ts
@@ -1,7 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable, take } from 'rxjs';
+
+interface ToDo {
+  id: number;
+  isCompleted: boolean;
+  description: string;
+}
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`
+  template: `
+    <div *ngFor="let todo of todos$ | async">
+      <input type="checkbox" [(ngModel)]="todo.isCompleted" />
+      {{ todo.description }}
+    </div>
+    `
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  todos$?: Observable<ToDo[]>;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.todos$ = this.http.get<ToDo[]>('/todos').pipe(take(1));
+  }
+}
