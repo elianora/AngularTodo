8cbf7f128b29f2b091a2dd52cb274ba800596025
Elianora Rose <developer@elianorarose.com>
2024-03-25 10:08:59 -0400

    Adding CRUD controller for ToDos

---
+++ AngularTodo.Server/DTOs/ToDoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AngularTodo.Server.DTOs;
+
+public class ToDoDto
+{
+    public uint? Id { get; set; }
+    public bool? IsCompleted { get; set; }
+    public string? Description { get; set; }
+}
