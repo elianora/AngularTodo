53151688376d99cdb13c6cb3b81f5ab7e4d636d3
Elianora Rose <developer@elianorarose.com>
2024-03-25 09:26:29 -0400

    Adding initial migrations

---
+++ AngularTodo.Server/Entities/ToDo.cs
@@ -0,0 +1,8 @@
+﻿namespace AngularTodo.Server.Entities;
+
+public class ToDo
+{
+    public uint Id { get; set; }
+    public bool IsCompleted { get; set; }
+    public required string Description { get; set; }
+}
